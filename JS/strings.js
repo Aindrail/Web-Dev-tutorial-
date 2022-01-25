487109571ba57e56507256c55c7da1c5705c724d
Aindrail <aindrailsantra@gmail.com>
2022-01-26 01:38:42 +0530

    String_basic_in JS

---
+++ JS/strings.js
@@ -0,0 +1,12 @@
+ let string = "hello" 
+ console.log(string);
+ let str = "Welcome"
+ let concat = string + str
+ console.log(concat);
+ let numStr = '123'
+ console.log(Number(numStr));
+ let num = 1234
+ console.log(num.toString());
+ let outMs = "hello" + 'welcome' ;
+ console.log(outMs); 
+ console.log(`${string + str} your score is ${numStr} `)
