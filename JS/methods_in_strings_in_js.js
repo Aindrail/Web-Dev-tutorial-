24f6816b0d844e53a7b1729c1f86d8f15418b3ea
Aindrail <aindrailsantra@gmail.com>
2022-01-26 02:05:10 +0530

    String_methods_in JS

--- JS/methods_in_strings_in_js.js
+++ JS/methods_in_strings_in_js.js
@@ -1,15 +1,15 @@
 var favShow = "Strangers Things";
-
+//legth of the string
 console.log(favShow.length)
-
+//char at index 0
 console.log(favShow[0]);
-
+//char a t index n-1
 console.log(favShow[favShow.length-1]);
-
+//return index of a char or starting index of a string
 console.log(favShow.indexOf("ers"));
-
+//return substring of desired indexes
 console.log(favShow.slice(5,9));
-
+//toupper and lower case conversion
 var srk ="My name is Khan";
 console.log(srk.toUpperCase());
 console.log(srk.toLowerCase());
