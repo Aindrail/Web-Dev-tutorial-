88ae88873a2f03f4787f4d797162136fcee7739e
Aindrail <aindrailsantra@gmail.com>
2022-01-26 02:04:04 +0530

    String_methods_in JS

---
+++ JS/methods_in_strings_in_js.js
@@ -0,0 +1,15 @@
+var favShow = "Strangers Things";
+
+console.log(favShow.length)
+
+console.log(favShow[0]);
+
+console.log(favShow[favShow.length-1]);
+
+console.log(favShow.indexOf("ers"));
+
+console.log(favShow.slice(5,9));
+
+var srk ="My name is Khan";
+console.log(srk.toUpperCase());
+console.log(srk.toLowerCase());
