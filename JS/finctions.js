11b2cc4ea25198fab72ec0dbc2e5e84805b105be
Aindrail <aindrailsantra@gmail.com>
2022-01-29 22:56:01 +0530

    Functions in JS

---
+++ JS/finctions.js
@@ -0,0 +1,19 @@
+//Functions
+
+//default parameter function
+function hello(){
+    console.log("Hello JS");
+}
+hello();
+
+//parameterised function
+function Name(name){
+    console.log("Nameste " + name);
+}
+Name("Aindrail");
+
+//return in function
+function addition(a,b){
+    return a+b;
+}
+console.log(addition(5,5));
