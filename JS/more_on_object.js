73ea11d6c6fd4bf3dcf59da46bf8b68b3da0ffd1
Aindrail <aindrailsantra@gmail.com>
2022-01-29 23:59:31 +0530

    difference between factory fun and constructer fun

--- JS/more_on_object.js
+++ JS/more_on_object.js
@@ -1,4 +1,22 @@
-const course ={
+// const course ={
+//     lecture: 18,
+//     section: 3,
+//     title: "JavaScript",
+//     notes:{
+// introduction: "Welcome to JS course"
+//     },
+//     enroll(){
+//         console.log('You are enrolled succesfully');
+//     }
+// }
+// course.enroll()
+// console.log(course.title)
+// course.price = 999
+// console.log(course)
+
+//Factory function
+function createC(){
+    return{
     lecture: 18,
     section: 3,
     title: "JavaScript",
@@ -9,8 +27,31 @@ introduction: "Welcome to JS course"
         console.log('You are enrolled succesfully');
     }
 }
-course.enroll()
-console.log(course.title)
+// return course;
+}
+const course = createC();
+course.enroll();
 
-course.price = 999
-console.log(course)
+function createC2(title){
+    return{
+    
+    title: "title",
+    enroll(){
+        console.log('You are enrolled succesfully');
+    }
+}
+// return course;
+}
+const course2 = createC2('Java Script');
+course2.enroll();
+
+//Constructor fun
+
+function Course3(title){
+    this.title = title,
+    this.enroll = function(){
+        console.log("You are great");
+    }
+}
+const course3 = new Course3('Javascript');
+course3.enroll()
