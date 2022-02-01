39702b6ed9ca68ba2e03a87913791582436298e2
Aindrail <aindrailsantra@gmail.com>
2022-02-01 10:06:56 +0530

    Class in JS

--- JS/class.js
+++ JS/class.js
@@ -9,13 +9,20 @@ class Prod {
 }
 let pencil = new Prod('Pencil',15,1,'P02');
 
-const Prod1 = class Prod {
+const Prod1 = class {
     constructor(name,price,discount,code){
         this.name = name;
         this.price = price;
         this.discount = discount;
         this.code = code;
 
+
+        }
+        get getDiscount(){
+            return this.discount;
+    }
+    set setDiscount( dis){
+   this.discount = dis;
     }
 };
 let chair = new Prod1('Chair',499,15,'C10');
