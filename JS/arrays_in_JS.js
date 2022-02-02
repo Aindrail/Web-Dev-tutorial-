efc6b3b16e4a065fb0ed4905514f75cbdba9fdda
Aindrail <aindrailsantra@gmail.com>
2022-02-03 00:10:09 +0530

    Arrays and push fun

--- JS/arrays_in_JS.js
+++ JS/arrays_in_JS.js
@@ -2,3 +2,12 @@
 let arr = ["Hello","Hi","Kemcho","Namaste","Nomoskar"]
 console.log(arr[3])
 console.log(arr.length)
+
+const array = [1,2,3,4,5,6,7,8,9]; 
+console.log(array);
+
+//push
+array.push(11); //pushes 11 in last and returns the new size of the array
+console.log(array.push(12 ));  
+
+//unshift
