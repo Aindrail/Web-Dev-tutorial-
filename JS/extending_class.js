8efdb8298f693c5cbb0dfead6aca6aa3095b164e
Aindrail <aindrailsantra@gmail.com>
2022-02-02 22:58:07 +0530

    extending class

---
+++ JS/extending_class.js
@@ -0,0 +1,21 @@
+class Product{
+    constructor(itemName){
+        this.itemName = itemName;
+    }
+    getItemName(){
+        return this.itemName + " is a Product";
+    }
+}
+class Furniture extends Product{
+    constructor(itemName){
+        super(itemName);
+    }
+    getItemName(){
+        return this.itemName + " is a furniture"
+    }
+} 
+
+let pencil = new Product('Pencil');
+let chair = new Product('Chair');
+console.log(pencil.getItemName());
+console.log(chair.getItemName());
