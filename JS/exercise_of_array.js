89136717cb9f3ae03bb02eb2adc29dcd30c83eec
Aindrail <aindrailsantra@gmail.com>
2022-02-06 23:28:25 +0530

    Array to display all  name

---
+++ JS/exercise_of_array.js
@@ -0,0 +1,45 @@
+let character = [
+    {
+        name : 'Dharmesh',
+        height : 168,
+        mass : 68,
+        eye_color : 'grey',
+        gender : 'male'
+    },{
+        name : 'Jay',
+        height : 198,
+        mass : 88,
+        eye_color : 'light-grey',
+        gender : 'male'
+    },{
+        name : 'pragati',
+        height : 162,
+        mass : 58,
+        eye_color : 'grey',
+        gender : 'female'
+    },{
+        name : 'Aakash',
+        height : 168,
+        mass : 68,
+        eye_color : 'black',
+        gender : 'male'
+    },{
+        name : 'Yagnesh',
+        height : 188,
+        mass : 60,
+        eye_color : 'blue',
+        gender : 'male'
+    }
+]
+
+ //Get an array of all names
+ const names = character.map((ch) =>{
+return ch.name
+ })
+ console.log(names);
+ //Get an array of objects with just name and height properties
+ //Get the total height of all characters
+ //Get characters with mass greater then 100
+ //Get all male characters
+ //Sort by genders
+ //Sort by names
